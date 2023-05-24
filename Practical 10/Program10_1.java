db9fc7c07505dc70aabc9f0731953a5f39969dd5
Madhav Kamble <madhavukamble@gmail.com>
2023-05-24 15:50:46 +0530

    File created for practical 10 and program 1 added

---
+++ Practical 10/Program10_1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+interface Gross{
+  double TA =10, DA=100 ;
+  double grossSalary();
+  
+}
+class Employee{
+  Scanner sc=new Scanner(System.in);
+  String name;
+  double base;
+  void basicSalary(){
+    System.out.println("enter your name");
+    name=sc.next();
+    System.out.println("enter the base salary");
+    base=sc.nextDouble();
+  }
+}
+class Salary extends Employee implements Gross{
+  Scanner sc=new Scanner(System.in);
+  double hra=20;
+ 
+ public double grossSalary(){
+    return (base+(base/10)+(base)+(base/5));
+  }
+  void displaySalary(){
+    basicSalary();
+    System.out.println("Total salary is: "+ grossSalary() );
+  }
+}
+class Program10_1{
+  public static void main(String args[]){
+    Salary s1=new Salary();
+    s1.displaySalary();
+  }
+}
