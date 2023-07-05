032fdb165fb63d89498beca8377bf1783ca2ea94
Madhav Kamble <madhavukamble@gmail.com>
2023-07-05 19:19:49 +0530

    QB Pattern 1 and 2 added(Pyramid)

--- POE QB/Pattern1.java
+++ POE QB/Pattern1.java
@@ -1,18 +1,18 @@
-/*Print following output using for loop:
+/*
 *
+**
 ***
+****
 *****
-*******
 */
 import java.util.*;
 class Pattern1{
-  static int row,i,j;
   public static void main(String args[]){
-    System.out.println("Enter the number of rows");
+    System.out.println("Enter number of rows");
     Scanner sc=new Scanner(System.in);
-    row= sc.nextInt();
-    for(i=1;i<=row;i++){
-      for(j=1;j<=i;j++){
+    int a=sc.nextInt();
+    for(int i=1;i<=a;i++){
+      for(int j=1;j<=i;j++){
         System.out.print(" * ");
       }
       System.out.println();
---
+++ POE QB/Pattern2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Pattern2{
+  public static void main(String args[]){
+    System.out.println("Enter the number of rows");
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    for(int i=1;i<=a;i++){
+      for(int k=a-i;k>=0;k--){
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print(" * ");
+      }
+      System.out.println();
+    }
+  }
+}
