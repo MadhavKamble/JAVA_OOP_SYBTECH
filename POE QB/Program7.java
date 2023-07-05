1f2d46befd0e407f9a044de1acd85b7ed4724634
Madhav Kamble <madhavukamble@gmail.com>
2023-07-05 23:43:35 +0530

    Updation in forest and department

--- POE QB/Program7.java
+++ POE QB/Program7.java
@@ -1,7 +1,7 @@
 /*Define a class ‘Department’ having data members: name_of_hod, 
-total_students and per_of_result. Define appropriate methods to initialize 
-and display the values of these data members. Input values for two objects 
-and determine whose average result per student is better.*/
+total_students and per_of_result. Define overloaded constructors to initialize 
+and method to display the values of these data members. Input values for 
+two objects and determine whose average result per student is worse*/
 import java.util.*;
 class Department{
   Scanner sc=new Scanner(System.in);
@@ -9,7 +9,9 @@ class Department{
   int total_students;
   double per_of_result;
   double condition=per_of_result/total_students;
-  void setData(){
+
+  Department(){
+    System.out.println("Enter the Data For First Department");
     System.out.println("Enter the name of HOD");
     name_of_hod=sc.nextLine();
     System.out.println("Enter total number of students");
@@ -17,7 +19,16 @@ class Department{
     System.out.println("Enter the per of result");
     per_of_result=sc.nextDouble();
   }
-  void getData(){
+
+  Department(String name_of_hod){
+    this.name_of_hod=name_of_hod;
+    System.out.println("Enter total number of students");
+    total_students=sc.nextInt();
+    System.out.println("Enter the per of result");
+    per_of_result=sc.nextDouble();
+  }
+
+  void display(){
     System.out.println("The name of the HOD is: "+ name_of_hod);
     System.out.println("Total number of students are: "+ total_students);
     System.out.println("Per of Result: "+ per_of_result);
@@ -25,12 +36,12 @@ class Department{
 }
 class Program7{
   public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
     Department d1=new Department();
-    Department d2= new Department(); 
-    d1.setData();
-    d1.getData();
-    d2.setData();
-    d2.getData();
+    System.out.println("Enter the Name of HOD for second Department");
+    String name =sc.nextLine();
+    Department d2= new Department(name);
+    
     if(d1.condition>d2.condition){
       System.out.println("First department is better");
     }
--- POE QB/Program8.java
+++ POE QB/Program8.java
@@ -1,36 +1,48 @@
 /*Define a class ‘Forest’ having data members: area_in_sq_cm, 
-count_of_animals. Define appropriate methods to initialize and display the 
-values of these data members. Input values for two objects and determine 
-which forest is having more space per animal.*/
+count_of_animals. Define overloaded constructors to initialize and method to 
+display the values of these data members. Input values for two objects and 
+determine which forest is having less space per animal.
+*/
 import java.util.*;
 class Forest{
+  Scanner sc=new Scanner(System.in);
   double area_in_sq_cm;
   int count_of_animals;
-  Scanner sc=new Scanner(System.in);
-  void setData(){
-    System.out.println("Enter the area in sq cm");
+  Forest(){
+    System.out.println("Enter the area of forest");
     area_in_sq_cm=sc.nextDouble();
-    System.out.println("Enter the number of animals");
+    System.out.println("Enter the count of animals");
     count_of_animals=sc.nextInt();
   }
-  void getData(){
-    System.out.println("The area is: "+area_in_sq_cm);
-    System.out.println("The count of animals is: "+count_of_animals);
+  Forest(double area_in_sq_cm){
+    this.area_in_sq_cm=area_in_sq_cm;
+    System.out.println("Enter the count of animals");
+    count_of_animals=sc.nextInt();
+  }
+  void display(){
+    System.out.println("Area of forest: "+area_in_sq_cm);
+    System.out.println("Total count of animals: "+count_of_animals);
   }
 }
 class Program8{
   public static void main(String args[]){
-    Forest amazon=new Forest();
-    Forest pandharpur=new Forest();
-    amazon.setData();
-    amazon.getData();
-    pandharpur.setData();
-    pandharpur.getData();
-    if((amazon.area_in_sq_cm/amazon.count_of_animals)>(pandharpur.area_in_sq_cm/amazon.count_of_animals)){
-      System.out.println("Amazon");
-    }
-    else{
-      System.out.println("Pandharpur");
-    }
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the data of First forest");
+    Forest f1=new Forest();
+    System.out.println("Enter the data of second forest");
+    System.out.println("Enter the area of forest");
+    double a=sc.nextDouble();
+    Forest f2=new Forest(a);
+    System.out.println("Data of first forest is: ");
+    f1.display();
+    System.out.println("Data of second forest is: ");
+    f2.display();
+  
+  if((f1.count_of_animals/f1.area_in_sq_cm)>(f2.count_of_animals/f2.area_in_sq_cm)){
+    System.out.println("First forest has less space per animal");
+  }
+  else{
+    System.out.println("Second forest has less space per animal");
+  }
   }
 }
